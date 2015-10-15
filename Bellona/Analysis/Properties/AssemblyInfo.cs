7b0e696f93574e0b806732565089403dabf8a439
Keiho Sakapon <git@saka-pon.net>
2015-10-15 17:18:29 +0900

    Update project settings

--- Bellona/Analysis/Properties/AssemblyInfo.cs
+++ Bellona/Analysis/Properties/AssemblyInfo.cs
@@ -38,7 +38,7 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("UnitTest")]
