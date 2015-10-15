85cc903b9047466d766b704cc7f5e816a5e4abab
Keiho Sakapon <git@saka-pon.net>
2015-10-15 17:17:14 +0900

    Add tools create packages

---
+++ Tools/CreatePackage.ps1
@@ -0,0 +1,13 @@
+﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+
+.\IncrementVersion-cs.ps1 ..\Bellona\Analysis
+
+$slnFilePath = "..\Bellona\Bellona.sln"
+Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Clean", $msbuild, $slnFilePath))
+Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Rebuild", $msbuild, $slnFilePath))
+
+cd ..\Bellona\Analysis
+.\NuGetPackup.exe
+
+move *.nupkg ..\..\Published -Force
+explorer ..\..\Published
