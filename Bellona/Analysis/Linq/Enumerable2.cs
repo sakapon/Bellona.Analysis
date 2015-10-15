7ff9705ce6def55243367d4167f8aef1e198e234
Keiho Sakapon <git@saka-pon.net>
2015-10-15 17:46:45 +0900

    Add methods

--- Bellona/Analysis/Linq/Enumerable2.cs
+++ Bellona/Analysis/Linq/Enumerable2.cs
@@ -6,6 +6,57 @@ namespace Bellona.Linq
 {
     public static class Enumerable2
     {
+        public static IEnumerable<TSource> ToEnumerable<TSource>(this TSource obj)
+        {
+            yield return obj;
+        }
+
+        public static IEnumerable<TSource> Do<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+
+            foreach (var item in source)
+            {
+                action(item);
+                yield return item;
+            }
+        }
+
+        public static void Execute<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            foreach (var item in source)
+            {
+            }
+        }
+
+        public static void Execute<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (action == null) throw new ArgumentNullException("action");
+
+            foreach (var item in source)
+            {
+                action(item);
+            }
+        }
+
+        public static IEnumerable<TSource> Distinct<TSource, TValue>(this IEnumerable<TSource> source, Func<TSource, TValue> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var valueSet = new HashSet<TValue>();
+
+            foreach (var item in source)
+            {
+                var value = selector(item);
+                if (!valueSet.Add(value)) continue;
+                yield return item;
+            }
+        }
+
         public static TSource FirstToMin<TSource>(this IEnumerable<TSource> source, Func<TSource, double> selector)
         {
             var o = source
