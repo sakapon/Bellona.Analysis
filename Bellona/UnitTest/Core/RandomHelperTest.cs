e5cf34da9638159da307b1fb29145aa2498c5d64
Keiho Sakapon <git@saka-pon.net>
2015-10-17 04:45:34 +0900

    Add methods

--- Bellona/UnitTest/Core/RandomHelperTest.cs
+++ Bellona/UnitTest/Core/RandomHelperTest.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bellona.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTest.Core
@@ -7,8 +10,30 @@ namespace UnitTest.Core
     public class RandomHelperTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShuffleRange()
         {
+            var set = new HashSet<int>(Enumerable.Range(0, 100));
+            var result = RandomHelper.ShuffleRange(100).ToArray();
+
+            Assert.IsTrue(set.SetEquals(result));
+        }
+
+        [TestMethod]
+        public void GetRandomPiece_1()
+        {
+            var array = Enumerable.Range(10, 10).ToArray();
+            var result = array.GetRandomPiece(3).ToArray();
+
+            Assert.AreEqual(3, result.Length);
+        }
+
+        [TestMethod]
+        public void GetRandomPiece_2()
+        {
+            var array = Enumerable.Range(10, 10).ToArray();
+            var result = array.GetRandomPiece(10).ToArray();
+
+            CollectionAssert.AreEqual(array, result);
         }
     }
 }
