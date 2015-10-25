5b7afc86ffef213d66d4068fd5fd6d516440e0e4
Keiho Sakapon <github@saka-pon.net>
2015-10-25 23:28:13 +0900

    Make ClusteringModel2 abstract

--- Bellona/Analysis/Analysis/Clustering/ClusteringModel2.cs
+++ Bellona/Analysis/Analysis/Clustering/ClusteringModel2.cs
@@ -13,7 +13,7 @@ namespace Bellona.Analysis.Clustering
             if (featuresSelector == null) throw new ArgumentNullException("featuresSelector");
             if (clustersNumber <= 0) throw new ArgumentOutOfRangeException("clustersNumber", clustersNumber, "The value must be positive.");
 
-            return new ClusteringModel2<T>(featuresSelector, clustersNumber, new Cluster<T>[0], new ClusteringRecord<T>[0]);
+            return new ClusteringModelForNumber<T>(featuresSelector, clustersNumber, new Cluster<T>[0], new ClusteringRecord<T>[0]);
         }
 
         public static ClusteringModel2<T> CreateFromStandardScore<T>(Func<T, ArrayVector> featuresSelector, double maxStandardScore = 1.645)
@@ -25,40 +25,51 @@ namespace Bellona.Analysis.Clustering
     }
 
     [DebuggerDisplay(@"\{Clusters={Clusters.Length}, Records={Records.Length}\}")]
-    public class ClusteringModel2<T>
+    public abstract class ClusteringModel2<T>
     {
-        Func<T, ArrayVector> _featuresSelector;
+        protected Func<T, ArrayVector> FeaturesSelector { get; private set; }
 
-        public int ClustersNumber { get; private set; }
         public Cluster<T>[] Clusters { get; private set; }
         public ClusteringRecord<T>[] Records { get; private set; }
 
-        public ClusteringModel2(Func<T, ArrayVector> featuresSelector, int clustersNumber, Cluster<T>[] clusters, ClusteringRecord<T>[] records)
+        protected ClusteringModel2(Func<T, ArrayVector> featuresSelector, Cluster<T>[] clusters, ClusteringRecord<T>[] records)
         {
-            _featuresSelector = featuresSelector;
-            ClustersNumber = clustersNumber;
+            FeaturesSelector = featuresSelector;
             Clusters = clusters;
             Records = records;
         }
 
-        public ClusteringModel2<T> Train(IEnumerable<T> source, int? maxIterations = null)
-        {
-            if (source == null) throw new ArgumentNullException("source");
-
-            var newRecords = source.Select(e => new ClusteringRecord<T>(e, _featuresSelector(e)));
-            var records = Records.Concat(newRecords).ToArray();
-
-            var initial = Clusters.Length > 0 ? Clusters : ClusteringHelper.InitializeClusters(ClustersNumber, records);
-            var clusters = ClusteringHelper.TrainIteratively(initial, records, maxIterations);
-
-            return new ClusteringModel2<T>(_featuresSelector, ClustersNumber, clusters, records);
-        }
+        public abstract ClusteringModel2<T> Train(IEnumerable<T> source, int? maxIterations = null);
 
         public Cluster<T> Assign(T element)
         {
             if (Clusters.Length == 0) throw new InvalidOperationException("This model is not trained.");
 
-            return ClusteringHelper.AssignFeatures(Clusters, _featuresSelector(element));
+            return ClusteringHelper.AssignFeatures(Clusters, FeaturesSelector(element));
+        }
+    }
+
+    public class ClusteringModelForNumber<T> : ClusteringModel2<T>
+    {
+        public int ClustersNumber { get; private set; }
+
+        public ClusteringModelForNumber(Func<T, ArrayVector> featuresSelector, int clustersNumber, Cluster<T>[] clusters, ClusteringRecord<T>[] records)
+            : base(featuresSelector, clusters, records)
+        {
+            ClustersNumber = clustersNumber;
+        }
+
+        public override ClusteringModel2<T> Train(IEnumerable<T> source, int? maxIterations = null)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            var newRecords = source.Select(e => new ClusteringRecord<T>(e, FeaturesSelector(e)));
+            var records = Records.Concat(newRecords).ToArray();
+
+            var initial = Clusters.Length > 0 ? Clusters : ClusteringHelper.InitializeClusters(ClustersNumber, records);
+            var clusters = ClusteringHelper.TrainIteratively(initial, records, maxIterations);
+
+            return new ClusteringModelForNumber<T>(FeaturesSelector, ClustersNumber, clusters, records);
         }
     }
 }
