8e220ac302f7516dcec0ec8542ad04bdfe206332
Keiho Sakapon <github@saka-pon.net>
2015-11-08 01:29:21 +0900

    Add cities data

--- Samples/ClusteringSample/CitiesWpf/AppModel.cs
+++ Samples/ClusteringSample/CitiesWpf/AppModel.cs
@@ -6,5 +6,11 @@ namespace CitiesWpf
 {
     public class AppModel
     {
+        public AppModel()
+        {
+            var CityType = EntityType.Create(new { CityName = "", Latitude = 0.0, Longitude = 0.0 });
+            var cities = CsvFile.ReadEntities("Cities.csv", CityType)
+                .ToArray();
+        }
     }
 }
