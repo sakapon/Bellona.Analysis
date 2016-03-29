3f9ade76bb8da0e9c832042eac9b944234466567
Keiho Sakapon <saka_pon@live.jp>
2016-03-29 13:11:14 +0900

    Update build tool

--- Tools/CreatePackage.ps1
+++ Tools/CreatePackage.ps1
@@ -1,10 +1,11 @@
-﻿$msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 
 .\IncrementVersion-cs.ps1 ..\Bellona\Analysis
 
 $slnFilePath = "..\Bellona\Bellona.sln"
-Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Clean", $msbuild, $slnFilePath))
-Invoke-Expression ([string]::Format("{0} {1} /p:Configuration=Release /t:Rebuild", $msbuild, $slnFilePath))
+& $msbuild $slnFilePath /p:Configuration=Release /t:Clean
+& $msbuild $slnFilePath /p:Configuration=Release /t:Rebuild
 
 cd ..\Bellona\Analysis
 .\NuGetPackup.exe
